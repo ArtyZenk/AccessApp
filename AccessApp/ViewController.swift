b54f32859c0480f262163fefff4442c245e86aa4
Artyom <artyomka13@gmail.com>
2022-05-26 13:40:49 +0300

    Initial Commit

---
+++ AccessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessApp
+//
+//  Created by Artyom Guzenko on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
