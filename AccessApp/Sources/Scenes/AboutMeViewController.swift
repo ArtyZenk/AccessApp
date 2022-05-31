64928d4dff03f80d237e6a9778cc4cf0c9ff4f0d
Artyom <artyomka13@gmail.com>
2022-05-31 11:02:19 +0300

    add new view controller

---
+++ AccessApp/Sources/Scenes/AboutMeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutMeViewController.swift
+//  AccessApp
+//
+//  Created by Artyom Guzenko on 31.05.2022.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
